e1e68da24a78e6c8afb782af5f02fe1ead8bdeba
catalinamatei <catalinamatei250@gmail.com>
2022-11-14 15:35:32 +0200

    Added  class UsersDTO for mapping class Users

---
+++ src/main/java/com/storemanagement/app/storemanagement/DTOs/UsersDTO.java
@@ -0,0 +1,15 @@
+package com.storemanagement.app.storemanagement.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersDTO {
+
+    private String title;
+    private String name;
+
+}
