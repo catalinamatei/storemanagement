59b30def3384f14b174e043eddbd2d319f7f4c2f
catalinamatei <catalinamatei250@gmail.com>
2022-11-18 12:29:47 +0200

    Created unit test for updateProduct method from class ProductsServices and added another product because product with name "apa" already exists and test addProductTest fails

--- src/test/java/com/storemanagement/app/storemanagement/Services/ProductsServiceTest.java
+++ src/test/java/com/storemanagement/app/storemanagement/Services/ProductsServiceTest.java
@@ -14,10 +14,19 @@ class ProductsServiceTest {
 
     @Test
     void addProductTest() throws Exception {
-        ProductsDTO productDTO = new ProductsDTO("bauturi", "apa", 2L);
+        ProductsDTO productDTO = new ProductsDTO("fructe", "piersici", 2L);
         productsService.addProduct(productDTO);
+        assertEquals( "piersici", productsService.getProductByName("piersici").getName());
+        assertEquals("fructe", productsService.getProductByName("piersici").getCategory());
+        assertEquals(2L, productsService.getProductByName("piersici").getPrice());
+    }
+
+    @Test
+    void updateProductTest() throws Exception{
+        ProductsDTO productDTO = new ProductsDTO("bauturi", "apa", 1L);
+        productsService.updateProduct(productDTO, "apa");
         assertEquals( "apa", productsService.getProductByName("apa").getName());
         assertEquals("bauturi", productsService.getProductByName("apa").getCategory());
-        assertEquals(2L, productsService.getProductByName("apa").getPrice());
+        assertEquals(1L, productsService.getProductByName("apa").getPrice());
     }
 }
