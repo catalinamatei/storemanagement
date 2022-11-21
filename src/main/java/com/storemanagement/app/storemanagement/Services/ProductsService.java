0c758381accdbb6178fe332a33ac91878ca5e2e7
catalinamatei <catalinamatei250@gmail.com>
2022-11-21 16:38:49 +0200

    Deleted empty line

--- src/main/java/com/storemanagement/app/storemanagement/Services/ProductsService.java
+++ src/main/java/com/storemanagement/app/storemanagement/Services/ProductsService.java
@@ -7,7 +7,6 @@ import com.storemanagement.app.storemanagement.Entities.Products;
 import com.storemanagement.app.storemanagement.Repositories.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
