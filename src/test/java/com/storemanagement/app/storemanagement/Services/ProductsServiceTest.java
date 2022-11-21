d055356b86a4f8fcccfcc012e63264731b8c624f
catalinamatei <catalinamatei250@gmail.com>
2022-11-21 15:13:14 +0200

    Added new test and fixed some variables.

--- src/test/java/com/storemanagement/app/storemanagement/Services/ProductsServiceTest.java
+++ src/test/java/com/storemanagement/app/storemanagement/Services/ProductsServiceTest.java
@@ -1,6 +1,7 @@
 package com.storemanagement.app.storemanagement.Services;
 
 import com.storemanagement.app.storemanagement.DTOs.ProductsDTO;
+import com.storemanagement.app.storemanagement.Entities.Products;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,20 +14,31 @@ class ProductsServiceTest {
     ProductsService productsService;
 
     @Test
+    public void findByNameTest(){
+        String name = "para";
+        Products product = productsService.getProductByName(name);
+        assertEquals("para", product.getName());
+        assertEquals("fructe", product.getCategory());
+        assertEquals(1L, product.getPrice());
+    }
+
+    @Test
     void addProductTest() throws Exception {
-        ProductsDTO productDTO = new ProductsDTO("fructe", "piersici", 2L);
+        String name = "apa minerala";
+        ProductsDTO productDTO = new ProductsDTO("bauturi", name, 2L);
         productsService.addProduct(productDTO);
-        assertEquals( "piersici", productsService.getProductByName("piersici").getName());
-        assertEquals("fructe", productsService.getProductByName("piersici").getCategory());
-        assertEquals(2L, productsService.getProductByName("piersici").getPrice());
+        assertEquals( name, productsService.getProductByName(name).getName());
+        assertEquals("bauturi", productsService.getProductByName(name).getCategory());
+        assertEquals(2L, productsService.getProductByName(name).getPrice());
     }
 
     @Test
     void updateProductTest() throws Exception{
-        ProductsDTO productDTO = new ProductsDTO("bauturi", "apa", 1L);
-        productsService.updateProduct(productDTO, "apa");
-        assertEquals( "apa", productsService.getProductByName("apa").getName());
-        assertEquals("bauturi", productsService.getProductByName("apa").getCategory());
-        assertEquals(1L, productsService.getProductByName("apa").getPrice());
+        String name = "para";
+        ProductsDTO productDTO = new ProductsDTO("fructe", name, 1L);
+        productsService.updateProduct(productDTO, "para ro");
+        assertEquals( name, productsService.getProductByName(name).getName());
+        assertEquals("fructe", productsService.getProductByName(name).getCategory());
+        assertEquals(1L, productsService.getProductByName(name).getPrice());
     }
 }
